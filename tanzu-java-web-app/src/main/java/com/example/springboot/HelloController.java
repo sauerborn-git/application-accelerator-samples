67e0b650b042a69e2debe6698d77936e2a3e4407
sauerborn-git <37106203+sauerborn-git@users.noreply.github.com>
2024-03-15 14:51:07 +0100

    Update HelloController.java

--- tanzu-java-web-app/src/main/java/com/example/springboot/HelloController.java
+++ tanzu-java-web-app/src/main/java/com/example/springboot/HelloController.java
@@ -8,7 +8,7 @@ public class HelloController {
 
 	@RequestMapping("/")
 	public String index() {
-		return "Hi! Greetings from Spring Boot + Tanzu! 2 =)";
+		return "Hi! Greetings from Spring Boot + Tanzu! 3 =)";
 	}
 
 }
